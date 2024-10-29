0aa9e193cdeeebbe2a3353c1146c174eebb9ac85
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2024-10-28 17:53:36 -0700

    Actions - using sequential and parallel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/Bromine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/Bromine.java
@@ -5,13 +5,15 @@ import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.New.Competition.subsystems.Claw;
 import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ClawRotater;
-import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ShoudlerJohn;
 import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ColorSensor;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ShoudlerJohn;
 
 public class Bromine {
     public Claw claw;
@@ -19,7 +21,7 @@ public class Bromine {
     public ShoudlerJohn shoulder;
     public ColorSensor colorSensor;
 
-    public Bromine(HardwareMap hardwareMap){
+    public Bromine(HardwareMap hardwareMap) {
         claw = new Claw(hardwareMap);
         clawRotater = new ClawRotater(hardwareMap);
         shoulder = new ShoudlerJohn(hardwareMap);
@@ -29,57 +31,69 @@ public class Bromine {
     double clawRotaterAngle;
     //TODO have a way to calculate the yaw of the claw with camera
 
-    public void update(Gamepad gamepad){
-        claw.update();
-        clawRotater.update(clawRotaterAngle);
-        shoulder.update();
-    }
-    public void update(){
+    public void update(Gamepad gamepad) {
         claw.update();
         clawRotater.update(clawRotaterAngle);
         shoulder.update();
     }
 
-    private class Intake implements Action {
+    public void update() {
+        claw.update();
+        clawRotater.update(clawRotaterAngle);
+        shoulder.update();
+    }
+
+    class Intake implements Action {
         boolean grabbingFromGround;
 
-        public Intake (boolean grabbingFromGround){
+        public Intake(boolean grabbingFromGround) {
             this.grabbingFromGround = grabbingFromGround;
         }
 
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
             claw.clawState = Claw.ClawState.OPEN;
-            if(grabbingFromGround){
+            if (grabbingFromGround) {
                 clawRotater.state = ClawRotater.State.ADJUSTING;
                 shoulder.state = ShoudlerJohn.State.GROUND;
-            }
-            else {
+            } else {
                 clawRotater.state = ClawRotater.State.INPUT;
                 shoulder.state = ShoudlerJohn.State.SUBMERSIBLE;
             }
 
-            if (colorSensor.checkForRecognition() == ColorSensor.Recognition.IN){
+            if (colorSensor.checkForRecognition() == ColorSensor.Recognition.IN) {
                 claw.clawState = Claw.ClawState.CLOSED;
                 return false;
-            }
-            else {
+            } else {
                 return true;
             }
         }
     }
-    private class Deposit implements Action{
-        @Override
-        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
 
-            //if done return false
+    ParallelAction driveAndRaise = new ParallelAction(
+            Positions.Rbasket.runTo,
+            telemetryPacket2 -> {
 
-            return true;
-        }
-    }
+                //This is a new action - place actual deposit work here
+
+                return true;
+            }
+    );
+
+    public SequentialAction depositBasket = new SequentialAction(
+            driveAndRaise,
+            telemetryPacket2 -> {
+
+
+                //Final Deposit
+
+
+                return true;
+            }
+    );
 
     public Action intake(boolean grabbingFromGround) {
         return new Intake(grabbingFromGround);
     }
-    public Action deposit(){return new Deposit();}
 }
