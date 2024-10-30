d01f79bd9c889d5896e4ba4bb4ef2f0ec441e3dc
nutella bananas <151780718+nutellabananas@users.noreply.github.com>
2024-10-29 22:22:22 -0700

    Update BromineDeposit.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/BromineDeposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/BromineDeposit.java
@@ -1,26 +1,25 @@
-package org.firstinspires.ftc.teamcode.New.Competition.subsystems;
+package org.firstinspires.ftc.teamcode.New.Competition.Actions;
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.ParallelAction;
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.New.Competition.Actions.Positions;
-import org.firstinspires.ftc.teamcode.New.Future.SubSystems.AttachmentsJohn;
-import org.firstinspires.ftc.teamcode.New.Future.SubSystems.LinearSlides;
-import org.firstinspires.ftc.teamcode.New.Future.SubSystems.Linkage;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.Claw;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ClawRotater;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ShoudlerJohn;
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.WristJohn;
 
-public class DepositShawty {
+public class DepositBromine{
 
     public Claw claw;
     public ClawRotater clawrotate;
     public Drive drive;
     public ShoudlerJohn shoudlerJohn;
-    public WristJohn  wrist;
+    public WristJohn wrist;
 
-    public DepositShawty (HardwareMap hardwareMap){
+    public DepositBromine (HardwareMap hardwareMap){
         claw = new Claw(hardwareMap);
         clawrotate = new ClawRotater(hardwareMap);
         drive = new Drive(hardwareMap);
@@ -36,7 +35,9 @@ public class DepositShawty {
             //todo- review claw rotate and fix which position its in
             clawrotate.state = ClawRotater.State.ZERO;
             shoudlerJohn.state = ShoudlerJohn.State.BASKET;
-            wrist.state = WristJohn.State.BASKET;
+            //idk why the state isn't public, it's public in shoulderjohn so should be accessible here.
+            // I'll fix it in the morning.
+            wrist.state = WristJohn.state.BASKET;
             //todo- add has reached function to shoudler john
             //idk how to see if it's reached postion, bc there isn't an encoder set up on ShoulderJohn
             if(shoudlerJohn.hasReached()) {
@@ -54,4 +55,4 @@ public class DepositShawty {
 
     }
 
-}
+
