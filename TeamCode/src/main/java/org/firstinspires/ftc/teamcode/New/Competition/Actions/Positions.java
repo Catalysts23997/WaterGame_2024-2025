d025959769736039eded54f0ec38f0466211b705
Tieg0sarus <Drake.A.Tieger@gmail.com>
2024-11-15 14:57:40 -0800

    Todo Review

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/Positions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Competition/Actions/Positions.java
@@ -6,36 +6,45 @@ import com.acmerobotics.roadrunner.Rotation2d;
 import com.acmerobotics.roadrunner.Vector2d;
 
 public enum Positions {
+    // Top Bricks
+    Blueleftbrick1 (new Vector2d(-68, 25.5), Rotation2d.fromDouble(0.0)),
+    Blueleftbrick2(new Vector2d(-58, 25.5), Rotation2d.fromDouble(0.0)),
+    Blueleftbrick3(new Vector2d(-48, 25.5), Rotation2d.fromDouble(0.0)),
 
-    //Bricks Y+B+R = colors L + R = Side of the map
-//        BLbrick1 (new Vector2d(-68, 25.5), Rotation2d.fromDouble(0.0)),
-//        BLbrick2(new Vector2d(-58, 25.5), Rotation2d.fromDouble(0.0)),
-//        BLbrick3(new Vector2d(-48, 25.5), Rotation2d.fromDouble(0.0)),
-    YLbrick1(new Vector2d(-68, -25.5), Rotation2d.fromDouble(0.0)),
-    YLbrick2(new Vector2d(-58, -25.5), Rotation2d.fromDouble(0.0)),
-    YLbrick3(new Vector2d(-48, -25.5), Rotation2d.fromDouble(0.0)),
-    //        YRbrick1 (new Vector2d(48, 25.5), Rotation2d.fromDouble(0.0)),
-//        YRbrick2(new Vector2d(58, 25.5), Rotation2d.fromDouble(0.0)),
-//        YRbrick3(new Vector2d(68, 25.5), Rotation2d.fromDouble(0.0)),
-    RRbrick1(new Vector2d(48, -25.5), Rotation2d.fromDouble(0.0)),
-    RRbrick2(new Vector2d(58, -25.5), Rotation2d.fromDouble(0.0)),
-    RRbrick3(new Vector2d(68, -25.5), Rotation2d.fromDouble(0.0)),
+    YellowRightbrick1 (new Vector2d(48, 25.5), Rotation2d.fromDouble(0.0)),
+    YellowRightbrick2(new Vector2d(58, 25.5), Rotation2d.fromDouble(0.0)),
+    YellowRightbrick3(new Vector2d(68, 25.5), Rotation2d.fromDouble(0.0)),
+
+    //Bottom Bricks
+    YellowLeftbrick1(new Vector2d(-68, -25.5), Rotation2d.fromDouble(0.0)),
+    YellowLeftbrick2(new Vector2d(-58, -25.5), Rotation2d.fromDouble(0.0)),
+    YellowLeftbrick3(new Vector2d(-48, -25.5), Rotation2d.fromDouble(0.0)),
+
+    RedRightbrick1(new Vector2d(48, -25.5), Rotation2d.fromDouble(0.0)),
+    RedRightbrick2(new Vector2d(58, -25.5), Rotation2d.fromDouble(0.0)),
+    RedRightbrick3(new Vector2d(68, -25.5), Rotation2d.fromDouble(0.0)),
+
     // point scoring spots
-//        Bbasket(new Vector2d(53, 53), Rotation2d.fromDouble(0.0)),
-    Rbasket(new Vector2d(-53, -53), Rotation2d.fromDouble(0.0)),
-    //        Bzone(new Vector2d(-57, 58), Rotation2d.fromDouble(0.0)),
-    Rzone(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
-    //        BSpecieminBar(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
-    RSpecieminBar(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
+    Bluebasket(new Vector2d(53, 53), Rotation2d.fromDouble(0.0)),
+    Redbasket(new Vector2d(-53, -53), Rotation2d.fromDouble(0.0)),
+
+    BlueHumanIntake(new Vector2d(-57, 58), Rotation2d.fromDouble(0.0)),
+    RedHumanIntake(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
+
+    BlueSpecieminBar(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
+    RedSpecieminBar(new Vector2d(57, -58), Rotation2d.fromDouble(0.0)),
+
     // Places around the center area
-//        TLwall(new Vector2d(-23.5, 23), Rotation2d.fromDouble(0.0)),
-//        TRwall(new Vector2d(23.5, 23), Rotation2d.fromDouble(0.0)),
-    BLwall(new Vector2d(-23.5, -23), Rotation2d.fromDouble(0.0)),
-    BRwall(new Vector2d(23.5, -23), Rotation2d.fromDouble(0.0)),
-    //        TLinpoint(new Vector2d(-22, 12.5), Rotation2d.fromDouble(-PI*0)),
-//        TRinpoint(new Vector2d(22, 12.5), Rotation2d.fromDouble(-PI*.5)),
-    BLinpoint(new Vector2d(-22, -12.5), Rotation2d.fromDouble(-PI * 0)),
-    BRinpoint(new Vector2d(22, -12.5), Rotation2d.fromDouble(-PI * .5));
+    TopLeftSidePanel(new Vector2d(-23.5, 23), Rotation2d.fromDouble(0.0)),
+    TopRightSidePanel(new Vector2d(23.5, 23), Rotation2d.fromDouble(0.0)),
+    BackLeftSidePanel(new Vector2d(-23.5, -23), Rotation2d.fromDouble(0.0)),
+    BackRightSidePanel(new Vector2d(23.5, -23), Rotation2d.fromDouble(0.0)),
+
+    // The center Submersable intake Points
+    TopLeftSubmersableIntake(new Vector2d(-22, 12.5), Rotation2d.fromDouble(-PI*0)),
+    TopRightSubmersableIntake(new Vector2d(22, 12.5), Rotation2d.fromDouble(-PI*.5)),
+    BackLeftSubmersableIntake(new Vector2d(-22, -12.5), Rotation2d.fromDouble(-PI * 0)),
+    BackRightSubmersableIntake(new Vector2d(22, -12.5), Rotation2d.fromDouble(-PI * .5));
 
     Positions(Vector2d vector, Rotation2d rotation2d) {
         runToNearest = new RunToNearest(vector);
