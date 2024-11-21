ec33546ee5fedb2738d9a5c15e98668a0cd1ec41
nutella bananas <151780718+nutellabananas@users.noreply.github.com>
2024-11-20 19:32:56 -0800

    blah blah

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Opmodes/Auto/TestingAuto.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Opmodes/Auto/TestingAuto.kt
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.New.Opmodes.Auto
-
 import com.acmerobotics.roadrunner.Action
 import com.acmerobotics.roadrunner.ParallelAction
 import com.acmerobotics.roadrunner.Pose2d
@@ -8,44 +7,45 @@ import com.acmerobotics.roadrunner.ftc.runBlocking
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
-//import org.firstinspires.ftc.teamcode.New.Old.Actions.Bromine
-import org.firstinspires.ftc.teamcode.New.Heisenberg.Actions.Positions
-import org.firstinspires.ftc.teamcode.New.Old.subsystems.Claw
-import org.firstinspires.ftc.teamcode.New.Old.subsystems.ShoudlerJohn
-import org.firstinspires.ftc.teamcode.New.Old.subsystems.WristJohn
+import org.firstinspires.ftc.teamcode.New.Competition.Actions.Bromine
+//import org.firstinspires.ftc.teamcode.New.Competition.Actions.Bromine
+//import org.firstinspires.ftc.teamcode.New.Competition.Actions.Bromine
+import org.firstinspires.ftc.teamcode.New.Competition.Actions.Positions
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.Claw
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ClawRotater
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ColorSensor
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.ShoudlerJohn
+import org.firstinspires.ftc.teamcode.New.Competition.subsystems.WristJohn
 import org.firstinspires.ftc.teamcode.New.PinpointLocalizer.Localizer
-
-class SampleAuto : LinearOpMode() {
+class TestingAuto : LinearOpMode() {
 
     override fun runOpMode() {
-
         val localizer = Localizer(hardwareMap, Localizer.Poses(0.0, 0.0, 0.0))
-//        val bromine = Bromine(hardwareMap);
+        val bromine = Bromine(hardwareMap);
         waitForStart()
-
         runBlocking(
             ParallelAction(
-                Action {
+                Action{
                     localizer.update()
                     false
+
                 },
                 SequentialAction(
 //                    bromine.prepareSpecimenDeposit,
-                    Positions.BlueSpecieminBar.runToExact,
+                    Positions.BlueSpecieminBar.runToNearest,
 ////                    bromine.fullSpecimenDeposit,
 
-                    //grabbing the specimens and giving them to Human PLayer x3
-//                    bromine.prepareSampleIntake,
+//                    //grabbing the specimens and giving them to Human PLayer x3
+////                    bromine.prepareSampleIntake,
                     Positions.Blueleftbrick1.runToExact,
-//                    bromine.SampleIntake,
-//                    bromine. prepForHPdrop,
-                    Positions.BlueHumanIntake.runToExact,
-//                    bromine.Drop,
+////                    bromine.SampleIntake,
+                    Positions.BlueHumanIntake.runToNearest,
+
 //                    bromine.prepareSampleIntake,
                     Positions.Blueleftbrick2.runToExact,
 //                    bromine.SampleIntake,
 //                    bromine.prepForHPdrop,
-                    Positions.BlueHumanIntake.runToExact,
+                    Positions.BlueHumanIntake.runToNearest,
 //                    bromine.Drop,
 //                    bromine.prepareSampleIntake,
                     Positions.Blueleftbrick3.runToExact,
@@ -56,28 +56,26 @@ class SampleAuto : LinearOpMode() {
 
                     //wall intakes and deposits
 //                    bromine.prepareSpecimenWallIntake,
+                    Positions.BlueHumanIntake.runToNearest,
+//                    bromine.SpecimenWallIntake,
+                    Positions.BlueHumanIntake.runToNearest,
+//                    bromine.prepareSpecimenDeposit,
+                    Positions.BlueSpecieminBar.runToNearest,
+//                    bromine.fullSpecimenDeposit,
+//                    bromine.prepareSpecimenWallIntake,
+//                    bromine.SpecimenWallIntake,
+//                    bromine.prepareSpecimenDeposit,
+//                    bromine.fullSpecimenDeposit,
+//                    bromine.prepareSpecimenWallIntake,
+//                    bromine.SpecimenWallIntake,
+//                    bromine.prepareSpecimenDeposit,
+//                    bromine.fullSpecimenDeposit,
                     Positions.BlueHumanIntake.runToExact,
-//                    bromine.SpecimenWallIntake,
-                    Positions.BlueHumanIntake.runToNearest,
-//                    bromine.prepareSpecimenDeposit,
-                    Positions.BlueSpecieminBar.runToExact,
-////                    bromine.fullSpecimenDeposit,
-//                    bromine.prepareSpecimenWallIntake,
-//                    bromine.SpecimenWallIntake,
-//                    bromine.prepareSpecimenDeposit,
-////                    bromine.fullSpecimenDeposit,
-//                    bromine.prepareSpecimenWallIntake,
-//                    bromine.SpecimenWallIntake,
-//                    bromine.prepareSpecimenDeposit,
-////                    bromine.fullSpecimenDeposit,
-                    Positions.BlueHumanIntake.runToNearest,
 
 
-                )
+                    )
             )
         )
 
-    }
 
-
-}
+    }    }
