6af05b2e46d47a740ede04e73d43acb70ab583a0
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2024-11-25 13:56:00 -0800

    Slides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/Subsystems/LinearSlides.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/Subsystems/LinearSlides.kt
@@ -1,109 +1,39 @@
-//package org.firstinspires.ftc.teamcode.New.Heisenberg.Subsystems;
-//
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//
-//import org.firstinspires.ftc.teamcode.New.PIDParams;
-//
-//public class LinearSlides {
-//
-//    public LeftSlide leftSlide;
-//    public RightSlide rightSlide;
-//    public int resetValue = 0;
-//
-//    public LinearSlides(HardwareMap hardwareMap){
-//        leftSlide = new LeftSlide(hardwareMap);
-//        rightSlide = new RightSlide(hardwareMap);
-//        leftSlide.leftSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        rightSlide.rightSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//    }
-//    public enum State{
-//        HANG(5000),
-//        BASKET(430),
-//        CLIP(340),
-//        INTAKE(550),
-//        STATIONARY(220),
-//        IDLE(0),
-//        Manual(0);
-//        State(int newEncoder){
-//            Encoder = newEncoder;
-//        }
-//        final int Encoder;
-//    }
-//
-//    public State state = State.IDLE;
-//    int target = 0;
-//
-//    public boolean hasReached(){
-//        int leftError = target - leftSlide.leftEncoder;
-//        int rightError = target - rightSlide.rightEncoder;
-//        int averageError = (rightError + leftError)/2;
-//        double tolerance = 0.01;
-//
-//        return averageError > -tolerance && averageError < tolerance;
-//    }
-//    public static int manualTarget = 0;
-//
-//    public void update(){
-//        target = state.Encoder;
-//        if(state == State.Manual){
-//            target = manualTarget;
-//        }
-//        leftSlide.update(target, state);
-//        rightSlide.update(target, state);
-//    }
-//
-//    public static class LeftSlide {
-//
-//        DcMotor leftSlide;
-//        public int leftEncoder;
-//
-//        //tune PID coefficients
-//        PIDParams pidParams = new PIDParams(0.0,0.0,0.0,0.0);
-//        PIDcontroller PIDcontroller = new PIDcontroller(pidParams);
-//
-//        public LeftSlide(HardwareMap hardwareMap){
-//            leftSlide = hardwareMap.get(DcMotor.class, "leftSlide");
-//        }
-//
-//
-//        public void update(int target, State state) {
-//            leftEncoder = leftSlide.getCurrentPosition();
-//
-//            double leftPower = PIDcontroller.calculate(target - leftEncoder);
-//
-//            //idle mode for testing or emergency stop
-//            if(state == State.IDLE){
-//               leftPower = 0;
-//           }
-//
-//            leftSlide.setPower(leftPower);
-//        }
-//    }
-//
-//    public static class RightSlide {
-//        DcMotor rightSlide;
-//        public int rightEncoder;
-//
-//        //tune PID coefficients
-//        PIDParams pidParams = new PIDParams(0.0,0.0,0.0,0.0);
-//        PIDcontroller PIDcontroller = new PIDcontroller(pidParams);
-//
-//        public RightSlide(HardwareMap hardwareMap){
-//            rightSlide = hardwareMap.get(DcMotor.class, "rightSlide");
-//        }
-//
-//        public void update(int target, State state) {
-//            rightEncoder = rightSlide.getCurrentPosition();
-//
-//            double rightPower = PIDcontroller.calculate(target - rightEncoder);
-//
-//            //idle mode for testing or emergency stop
-//            if(state == State.IDLE){
-//                rightPower = 0;
-//            }
-//
-//            rightSlide.setPower(rightPower);
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode.New.Heisenberg.Subsystems
+
+import com.qualcomm.robotcore.hardware.DcMotorEx
+import com.qualcomm.robotcore.hardware.HardwareMap
+import org.firstinspires.ftc.teamcode.New.Controller
+import org.firstinspires.ftc.teamcode.New.PIDParams
+
+class LinearSlides(val hwMap:HardwareMap) {
+
+    val leftMotor = hwMap.get(DcMotorEx::class.java, "leftSlide")
+    val rightMotor = hwMap.get(DcMotorEx::class.java, "rightMotor")
+
+
+    //todo: Create an opmode that uses this subsytem - and sets the pid controller's paramters to ftc Dashboard numbers
+
+    val pidController = Controller(PIDParams(0.0,0.0,0.0,0.0))
+
+    fun setPID(p: Double, i: Double, d: Double, f: Double) {
+        pidController.params = PIDParams(p,i,d,f)
+    }
+    enum class State(val encoder: Int) {
+        HANG(5000),
+        BASKET(430),
+        CLIP(340),
+        INTAKE(550),
+        STATIONARY(220),
+        IDLE(0),
+        Manual(0)
+    }
+
+    var state = State.IDLE
+
+    fun update(){
+        val effort = pidController.calculate(state.encoder.toDouble())
+        leftMotor.power = effort
+        rightMotor.power = -effort
+    }
+
+}
