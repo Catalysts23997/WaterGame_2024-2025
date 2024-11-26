10679270dbf1fd71d31e0b2deb122212f77b37a9
nutella bananas <151780718+nutellabananas@users.noreply.github.com>
2024-11-25 17:40:47 -0800

    fixed motor...again?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/OpModes/Testing/LinerSlidesTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/OpModes/Testing/LinerSlidesTesting.java
@@ -1,30 +1,27 @@
 package org.firstinspires.ftc.teamcode.New.Heisenberg.OpModes.Testing;
 
-import androidx.annotation.NonNull;
-
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 @Config
 @TeleOp(name = "LinerSlidesTesting", group = "Linear OpMode")
 public class LinerSlidesTesting extends LinearOpMode {
     public static double motorpose=0.0;
-    DcMotor leftSlide;
-    DcMotor rightslide;
-    public LinerSlidesTesting(HardwareMap hardwareMap){
-        leftSlide = hardwareMap.get(DcMotor.class, "leftslide");
-        rightslide = hardwareMap.get(DcMotor.class, "rightslide");}
+//    DcMotor leftslide;
+//    DcMotor rightslide;
+
+    DcMotor leftslide = hardwareMap.get(DcMotorEx.class, "leftslide");
+    DcMotor rightslide = hardwareMap.get(DcMotorEx.class, "rightslide");
+
+
     @Override
     public void runOpMode() throws InterruptedException {
         rightslide.getCurrentPosition();
-        leftSlide.getCurrentPosition();
-        double leftpos = leftSlide.getCurrentPosition();
+        leftslide.getCurrentPosition();
+        double leftpos = leftslide.getCurrentPosition();
         double rightpos = rightslide.getCurrentPosition();
         waitForStart();
         while (opModeIsActive()){
