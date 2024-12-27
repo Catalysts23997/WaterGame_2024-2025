9763dffa6c150f3c8f1de946456e77a995db57ee
Vratesh Pandey- Catalysts <146493546+vrateshpandey@users.noreply.github.com>
2024-12-26 18:54:23 -0800

    vrateshRaiseandDropinbasket.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/Actions/vrateshRaiseandDropinbasket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/Actions/vrateshRaiseandDropinbasket.java
@@ -6,23 +6,19 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 
 public class vrateshRaiseandDropinbasket {
-}
-public Action raisesanddropspixel = new Action() {
-    @Override
-    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+
+    public Action raisesanddropspixel = new Action() {
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
 
 
-        //raiselinearslides
-        //shoulder
-        //drop using wrist
-        //claw release
+            //raiselinearslides
+            //shoulder
+            //drop using wrist
+            //claw release
 
 
-
-
-
-
-
-        return false;
-    }
+            return false;
+        }
+    };
 }
