20386c74ea0c2a57eabdb2e60b0109fc14103f8d
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2025-01-21 17:56:02 -0800

    Adding the resources needed to hold robot position during PID auto, otherwise robot would drift over time

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/New/Heisenberg/OpModes/Testing/Auto/AutoNewWork.kt
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.New.Heisenberg.OpModes.Testing.Auto
+
+//import org.firstinspires.ftc.teamcode.New.Competition.Actions.Bromine
+//import org.firstinspires.ftc.teamcode.New.Competition.Actions.Bromine
+import com.acmerobotics.roadrunner.Action
+import com.acmerobotics.roadrunner.ParallelAction
+import com.acmerobotics.roadrunner.SequentialAction
+import com.acmerobotics.roadrunner.ftc.runBlocking
+import com.qualcomm.robotcore.eventloop.opmode.Disabled
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.New.Heisenberg.Pathing.RunToExact
+import org.firstinspires.ftc.teamcode.New.Heisenberg.Pathing.SetDriveTarget
+import org.firstinspires.ftc.teamcode.New.PinpointLocalizer.Localizer
+import org.firstinspires.ftc.teamcode.New.Utilities.Poses
+
+@Disabled
+class AutoNewWork : LinearOpMode() {
+    companion object{
+        var robot_targetPosition = Poses(-31.0, -65.0, 0.0)
+    }
+    override fun runOpMode() {
+
+        val localizer = Localizer(hardwareMap, robot_targetPosition)
+
+
+        waitForStart()
+        runBlocking(
+            ParallelAction(
+                Action {
+                    localizer.update()
+                    true
+                },
+
+                SequentialAction(
+                    SetDriveTarget(Poses(0.0,0.0,0.0)),
+                    SetDriveTarget(Poses(0.0,0.0,0.0)),
+                    SetDriveTarget(Poses(0.0,0.0,0.0)),
+                    SetDriveTarget(Poses(0.0,0.0,0.0))
+                ),
+
+                Action {
+                    RunToExact(robot_targetPosition)
+                    true
+                }
+            )
+        )
+
+
+    }
+}
